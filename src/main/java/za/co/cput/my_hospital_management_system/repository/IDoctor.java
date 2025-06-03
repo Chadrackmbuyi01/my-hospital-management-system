6784f9ed8dff5d286614dcadb4dd0c29007f4c82
Chadrack Mbuyi Kalala <80404784+Chadrackmbuyi01@users.noreply.github.com>
2025-06-03 21:59:53 +0200

    Creating repository interface of each entity

---
+++ src/main/java/za/co/cput/my_hospital_management_system/repository/IDoctor.java
@@ -0,0 +1,7 @@
+package za.co.cput.my_hospital_management_system.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.co.cput.my_hospital_management_system.model.Doctor;
+
+public interface IDoctor extends JpaRepository<Doctor,Long> {
+}
