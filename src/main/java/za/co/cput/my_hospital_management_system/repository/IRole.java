24b27f341b3e7c3376bf493d198afb2cdb124a51
Chadrack Mbuyi Kalala <80404784+Chadrackmbuyi01@users.noreply.github.com>
2025-06-03 22:07:14 +0200

    Repository layer has been created successfully

---
+++ src/main/java/za/co/cput/my_hospital_management_system/repository/IRole.java
@@ -0,0 +1,7 @@
+package za.co.cput.my_hospital_management_system.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.co.cput.my_hospital_management_system.model.Role;
+
+public interface IRole extends JpaRepository<Role,Long> {
+}
